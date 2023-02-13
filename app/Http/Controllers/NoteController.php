8ae4484de14075a72e4770e635559306d6dc6288
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-02-13 13:29:24 +0700

    use middleware auth

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(Request $request)
     {
         if ($request->search) {
