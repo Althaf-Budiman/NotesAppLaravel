1234958b7383789846222a72120d01f90f3aa2d7
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-02-13 09:35:44 +0700

    added edit feature

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -11,14 +11,14 @@
     <div class="flex flex-row flex-wrap mt-5 mx-8 justify-center gap-10">
         @foreach ($data as $item)
             <form action="{{ url("/notes/$item->id") }}" method="POST">
-                <a href="{{ url("/notes/$item->edit") }}">
+                <a href="{{ url("/notes/$item->id/edit") }}">
                     <div class="card w-96 bg-slate-800 hover:bg-slate-900 transition shadow-2xl">
                         <div class="card-body">
                             <h1 class="card-title">{{ $item->title }}</h1>
                             <p class="max-h-32 overflow-y-auto mt-3">{{ $item->note }}</p>
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn btn-outline btn-error btn-block">
+                            <button type="submit" class="btn btn-outline btn-error btn-block mt-3">
                                 <p>Delete</p>
                             </button>
                         </div>
