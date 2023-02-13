75b5da62f7599eadbad4dbd78a09d61ecee1d405
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-02-13 12:03:41 +0700

    add register feature

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,8 @@
-<div class="navbar bg-slate-800 sticky top-0">
-  <a href="{{ url('/') }}" class="btn btn-ghost normal-case text-xl">Notes App</a>
+<div class="navbar bg-slate-800 sticky top-0 z-20">
+  <div class="navbar-start">
+    <a href="{{ url('/') }}" class="btn btn-ghost normal-case text-xl">Notes App</a>
+  </div>
+  <div class="navbar-end">
+    <a href="{{ route('register') }}" class="btn btn-outline btn-warning mr-5">Register</a>
+  </div>
 </div>
