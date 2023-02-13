811de7b943b347dc413aa8133170b73552b9a400
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-02-13 10:25:31 +0700

    adding fortify to the project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Notes App</title>
+    <link rel="shortcut icon" href="" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/daisyui@2.50.0/dist/full.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
